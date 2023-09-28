8817170b0834edf0c6be8a05d5ddbc8153469d4c
XabierMatute <109271435+XabierMatute@users.noreply.github.com>
2023-09-28 15:33:14 +0200

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -6,89 +6,71 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 15:32:05 by xmatute-          #+#    #+#             */
-/*   Updated: 2022/09/22 19:20:22 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/09/27 11:04:06 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-static unsigned int	palabras(char const *s, char c)
+static void	ft_free2(void **ss)
 {
-	unsigned int	p;
+	size_t	i;
+
+	i = 0;
+	while (ss[i])
+	{
+		free(ss[i]);
+		i++;
+	}
+	free(ss);
+}
+
+static size_t	wlen(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+static size_t	palabras(char const *s, char c)
+{
+	size_t	p;
 
 	p = 0;
-	while (*(s++))
-		if ((*s == 0 || *s == c) && *(s - 1) != c)
+	while (*s)
+	{
+		if (s[0] != c && (s[1] == c || s[1] == 0))
 			p++;
+		s++;
+	}
 	return (p);
 }
 
-static size_t	ft_wlen(const char *s, char c)
-{
-	int	l;
-
-	l = 0;
-	while (s[l] && s[l] != c)
-		l++;
-	return (l);
-}
-
-static char	**liberate(char ***f, unsigned int p)
-{
-	unsigned int	i;
-	char			**sl;
-
-	i = 0;
-	sl = *f;
-	while (i < p && sl[i])
-	{
-		free(sl[i]);
-		i++;
-	}
-	free(sl);
-	return (NULL);
-}
-
-// static char	**falla(char **sl, unsigned int p)
-// {
-// 	unsigned int	i;
-
-// 	i = 0;
-// 	while (i < p)
-// 	{
-// 		if (!sl[i])
-// 		{
-// 			liberate(&sl, p);
-// 			return (NULL);
-// 		}
-// 		i++;
-// 	}
-// 	return (sl);
-// }
-
 char	**ft_split(char const *s, char c)
 {
-	char			**sl;
-	unsigned int	i;
-	unsigned int	p;
+	char	**ss;
+	size_t	i;
+	size_t	j;
 
-	sl = malloc(sizeof(char *) * (palabras(s, c) + 1));
-	if (!sl)
-		return (0);
+	ss = malloc(sizeof(char *) * (palabras(s, c) + 1));
+	if (!ss)
+		return (NULL);
 	i = 0;
-	p = 0;
-	while (s[i] == c && s[i])
-		i++;
-	while (s[i])
+	j = 0;
+	while (i < palabras(s, c))
 	{
-		sl[p] = ft_substr(s, i, ft_wlen(s + i, c));
-		if (!sl[p])
-			return (liberate(&sl, p));
-		p++;
-		i = i + ft_wlen(s + i, c);
-		while (s[i] == c && s[i])
-			i++;
+		while (s[j] == c)
+			j++;
+		ss[i] = ft_substr(s, j, wlen(s + j, c));
+		if (!ss[i])
+			return (ft_free2((void **)ss), NULL);
+		i++;
+		j += wlen(s + j, c);
 	}
-	sl[p] = 0;
-	return (sl);
+	ss[i] = NULL;
+	return (ss);
 }
