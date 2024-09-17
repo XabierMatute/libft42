ebfd812d8ef05c9be91d538ca56fa244a0354b14
xmatute- <xmatute-@student.42urduliz.com>
2024-09-17 23:06:32 +0200

    añadido eprintf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 20:52:04 by xmatute-          #+#    #+#             */
-/*   Updated: 2024/09/17 22:43:34 by xmatute-         ###   ########.fr       */
+/*   Updated: 2024/09/17 23:05:16 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,25 @@ int ft_dprintf(int fd, char const *s, ...)
 			printed_chars += ft_putva_fd(va, *(++s), fd);
 		else
 			printed_chars += ft_putchar_fd(*s, fd);
+		s++;
+	}
+	va_end(va);
+	return(printed_chars);
+}
+
+int ft_eprintf(char const *s, ...)
+{
+	int	printed_chars;
+	va_list	va;
+
+	printed_chars = 0;
+	va_start(va, s);
+	while(*s)
+	{
+		if (*s == '%')	
+			printed_chars += ft_putva_fd(va, *(++s), 2);
+		else
+			printed_chars += ft_putchar_fd(*s, 2);
 		s++;
 	}
 	va_end(va);
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 21:43:39 by xmatute-          #+#    #+#             */
-/*   Updated: 2024/09/17 22:41:52 by xmatute-         ###   ########.fr       */
+/*   Updated: 2024/09/17 23:05:34 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 int ft_printf(const char *format, ...);
 int ft_dprintf(int fd, const char *format, ...);
+int ft_eprintf(const char *format, ...);
 
 #endif //FT_PRINTF_H
