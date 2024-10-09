a1d562d65d8d5a78ea4e8d057282808bbb772b66
xmatute- <xmatute-@student.42urduliz.com>
2024-10-09 18:11:55 +0200

    faltaba un ; jeje

--- ft_swap.h
+++ ft_swap.h
@@ -6,13 +6,13 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 22:10:38 by xmatute-          #+#    #+#             */
-/*   Updated: 2024/10/09 18:06:07 by xmatute-         ###   ########.fr       */
+/*   Updated: 2024/10/09 18:11:26 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_SWAP_H
 #define FT_SWAP_H
 
-void ft_swap_p(void **p1, void **p2)
+void ft_swap_p(void **p1, void **p2);
 
 #endif //FT_SWAP_H
