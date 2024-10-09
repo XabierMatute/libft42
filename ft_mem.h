bbdeda6a5bd55f9b9a633193093d10c66dfb0937
xmatute- <xmatute-@student.42urduliz.com>
2024-10-09 18:21:47 +0200

    ft_free2 añadido

--- ft_mem.h
+++ ft_mem.h
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 22:15:00 by xmatute-          #+#    #+#             */
-/*   Updated: 2024/09/17 22:15:38 by xmatute-         ###   ########.fr       */
+/*   Updated: 2024/10/09 18:17:45 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+
+void	free2(void **pp);
 
 #endif //FT_MEM_H
